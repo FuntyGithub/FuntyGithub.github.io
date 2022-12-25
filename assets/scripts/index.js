1a01295ab59aa65ddb0d3c46a100d1466de2c737
FuntyGithub <74513280+FuntyGithub@users.noreply.github.com>
2022-12-25 17:46:33 +0100

    dont save theme cookie global .-.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -224,14 +224,21 @@ function themeSearch(text) {
 }
 
 // cookies
-function setCookie(name, value, days) {
+function setCookie(name, value, days, global) {    
+
+    let path = "; path=" + window.location.pathname;
+    if (global) {
+        path = "; path=/";
+    }
+
+
     let expires = "";
     if (days) {
         let date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + path;
 }
 
 function getCookie(name) {
